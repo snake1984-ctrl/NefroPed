b951aa81a005f9badd01fcc1f55c579d34f78ba9
snake1984-ctrl <snake1984@gmail.com>
2026-03-09 21:15:13 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nefroped-v22'; // <--- ACUÉRDATE DE CAMBIAR ESTO AL SUBIR
+const CACHE_NAME = 'nefroped-v23'; // <--- ACUÉRDATE DE CAMBIAR ESTO AL SUBIR
 
 const urlsToCache = [
   './',
@@ -53,6 +53,7 @@ self.addEventListener('fetch', event => {
       .catch(() => caches.match(event.request).then(res => res || caches.match('./index.html')))
   );
 });
+
 
 
 
